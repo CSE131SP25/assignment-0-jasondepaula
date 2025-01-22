3893e159f79627704ffd4d8772c8bc71262dedbf
jasonesc <jasonesc@localhost>
2025-01-22 13:08:47 -0600

    d

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -34,6 +34,11 @@ public class RobotInstructions  {
 		robot.forward(23);
 		robot.turnRight(90);
 		robot.forward(80);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(23);
+		robot.turnRight(90);
+		robot.forward(80);
 	}
 
 }
